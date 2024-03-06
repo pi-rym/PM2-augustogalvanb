e48f5a94f8cd05830caf6d284807710aafbf0feb
augustogb <august_gb2@outlook.com>
2024-03-05 22:32:39 -0300

    commit homework2 pi2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,45 +1,52 @@
-// const tempData = [
-//     {
-//       title: "Guardians of the Galaxy Vol. 2",
-//       year: 2017,
-//       director: "James Gunn",
-//       duration: "2h 16min",
-//       genre: ["Action", "Adventure", "Comedy"],
-//       rate: 7.7,
-//       poster:
-//         "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
-//     },
-//     {
-//       title: "Star Wars: Episode IV - A New Hope",
-//       year: 1977,
-//       director: "George Lucas",
-//       duration: "2h 1min",
-//       genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
-//       rate: 8.7,
-//       poster:
-//         "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-//     },
-//     {
-//       title: "The Lord of the Rings: The Fellowship of the Ring",
-//       year: 2001,
-//       director: "Peter Jackson",
-//       duration: "2h 58min",
-//       genre: ["Action", "Adventure", "Drama", "Fantasy"],
-//       rate: 8.8,
-//       poster:
-//         "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-//     },
-//   ];
+class Pelicula {
+    constructor(poster, title) {
+      this.poster = poster;
+      this.title = title;
+    }
+  }
 
-// import tempData from "./tempData";
+  class Repositorio {
+    constructor() {
+      this.peliculas = [];
+    }
+  
+    crearPelicula({ poster, title }) {
+      const nuevaPelicula = new Pelicula(poster, title);
+      this.peliculas.push(nuevaPelicula);
+    }
+  
+    darPeliculas() {
+      return this.peliculas;
+    }
+  }
 
-// console.log(tempData);
+const repositorio = new Repositorio();
 
 
 
+function solicitudGet ()  {
+    $.get(`https://students-api.2.us-1.fl0.io/movies`, infoPeliculas);
+}
 
-const contenedorPeliculas = document.getElementById('contenedorPeliculas');
-tempData.forEach(pelicula => {
+
+const infoPeliculas = (data) => {
+    data.forEach(pelicula => {
+        repositorio.crearPelicula(pelicula);
+    });
+    renderPeliculas();
+};
+
+const renderPeliculas = () => {
+    const peliculas = repositorio.darPeliculas();
+
+    const TodasLasPeliculas = peliculas.map(mapearPeliculas);
+
+    TodasLasPeliculas.forEach((pelicula) => contenedorPeliculas.appendChild(pelicula));
+}
+
+const mapearPeliculas = (pelicula) => {
+    const contenedorPeliculas = document.getElementById('contenedorPeliculas');
+
     const nuevoDiv = document.createElement('div');
     const imagenPelicula = document.createElement('img');
     const nombrePelicula = document.createElement('h4');
@@ -54,5 +61,9 @@ tempData.forEach(pelicula => {
     
     nuevoDiv.appendChild(imagenPelicula);
     nuevoDiv.appendChild(nombrePelicula);
-    contenedorPeliculas.appendChild(nuevoDiv);
-});
+    return nuevoDiv;
+    
+}
+
+solicitudGet()
+
