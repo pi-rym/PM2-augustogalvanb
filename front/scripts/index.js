69202c765d5bef1e30cba5b8bb7f80cae2e7a921
augustogb <august_gb2@outlook.com>
2024-03-13 00:38:43 -0300

    commit actualizado con tarea del servidor (express) y las solicitudes http con promesas

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,7 +1,44 @@
-const renderMovies = require("./renderMovies")
+// const renderMovies = require("./renderMovies")
 
-function solicitudGet ()  {
-  $.get(`https://students-api.up.railway.app/movies`, renderMovies);
+// function solicitudGet ()  {
+//   $.get(`https://students-api.up.railway.app/movies`, renderMovies);
+// }
+
+// solicitudGet()
+
+//solicitud HTTP GET a la URL de películas utilizando la librería axios.
+//manejo de la asincronía utilizando promesas
+//las promesas representan el eventual resultado de una operación asíncrona.
+const axios = require("axios");
+
+// const promise = axios.get("https://students-api.up.railway.app/movies");
+
+// //succes handler
+// promise
+// .then((response) => {
+//   response.data;
+// })
+// .catch((error) => {
+//   error
+// })
+//error handler
+
+//
+
+//manejo de la asincronía utilizando async/await: define una funcion de tipo async
+
+const fetchMovies = async () => {
+  try {
+    const data = await axios.get("https://students-api.up.railway.app/movies")
+    data.data;
+  } catch (error) {
+    error.message
+  }
+  
 }
 
-solicitudGet()
+fetchMovies();
+
+
+
+
